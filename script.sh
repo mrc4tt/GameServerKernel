ad3335886f32a83374a6d48526fb49bd9e8a4c71
Mikkel <mikkel.linddk@gmail.com>
2020-01-07 19:34:39 +0100

    Update script.sh
    Updated.

--- script.sh
+++ script.sh
@@ -2,36 +2,37 @@
 # __________________________________________________________________________________________
 # ******  Priority Servers. - ( Add other Server Process names, u can add here.) ***********
 PROCESS_NAMES=(
+  srcds_run
   srcds_linux
 );
 
-for pid in $(pidof ${PROCESS_NAMES[*]}) ;do
+for pid in $(for executable in ${PROCESS_NAMES[@]}; do pgrep ${executable} ;done) ;do
   chrt -ar -p 98 $pid
-  renice -20 -p $pid
+  renice 1 -p $pid
 done
 
 # __________________________________________________________________________________________
-# ****** NETWORK Optimizer for KERNEL. - (no-delay data transfer.) **********************
+# ****** NETWORK Optimizer for RT KERNEL. - (no-delay data transfer.) **********************
 for pid in $(ps ax | grep -E "\s+\[irq\/[0-9]+-\S+[0-9]+(-.+|)\]$" | awk '{print $1}'); do
   chrt -r -p 98 $pid
   renice 1 -p $pid
 done
 
 # __________________________________________________________________________________________
-# ****** ksoftirqd scheduling Optimizer for KERNEL. - ( ) *******************************
+# ****** ksoftirqd scheduling Optimizer for RT KERNEL. - ( ) *******************************
 for pid in $(pgrep ksoftirqd); do
-  chrt -f -p 50 $pid
+  chrt -r -p 50 $pid
 done
 
 # __________________________________________________________________________________________
-# ****** kworker scheduling Optimizer for KERNEL. - ( ) *********************************
+# ****** kworker scheduling Optimizer for RT KERNEL. - ( ) *********************************
 PIDS=`ps ax | grep kworker | grep -v grep | sed -e "s/^ *//" -e "s/ .*$//"`
 for p in $PIDS; do
   renice -20 -p $p
 done
 
 # __________________________________________________________________________________________
-# ****** TOP Process Optimizer for KERNEL. - ( ) ****************************************
+# ****** TOP Process Optimizer for RT KERNEL. - ( ) ****************************************
 for pid in $(pidof top); do
-  chrt -ar -p 54 $pid
+  chrt -af -p 54 $pid
 done
